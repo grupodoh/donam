be7944e7b83f1bda6094880e7ce3fbf6e5d937f6
Juan Diego Moncaleano Quiñones <juan.moncaleano@interedes.com.co>
2023-09-08 09:02:17 -0500

    Se cre la interfaz grafica del inicio del aplicativo

--- index.php
+++ index.php
@@ -1,45 +1,57 @@
-<?php
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    require_once 'Usuario.php';
-
-    $usuario = new Usuario();
-    $resultado = $usuario->iniciarSesion($_POST['usuario'], $_POST['contrasena']);
-
-    if ($resultado) {
-        header('Location: dashboard.php');
-        exit();
-    } else {
-        $mensajeError = "Credenciales incorrectas. Por favor, intenta de nuevo.";
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+    <title>Doña M</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="css/estilos.css" rel="stylesheet" type="text/css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
 </head>
 <body>
-   <div class="contenedor">
-    
-    <div id="login">
-    <?php if (isset($mensajeError)) { echo "<p>$mensajeError</p>"; } ?>
-    <p><b>Inicio de Sesion</b></p><br>
-        <form method="POST" action="inicio.php">
-            <br>
-            <input type="text" placeholder="Usuario" class="form-control" name="usuario" require="true"><br>
-            <br>
-            <input type="password" placeholder="Contraseña" class="form-control" name="contrasena" require="true"><br>
-            <input type="button" value="Iniciar sesión" class="btn-primary">
-        </form>
+    <div class="container d-flex justify-content-center align-items-center" style="height:100vh;">
+        <div class="">
+            <div class="card shadow-lg quitar-bordes">
+                <div class="card-body">
+                    <div class="row my-5 mx-4">
+                        <div class="col-12">
+                            <h5>Inicio de sesión</h5>
+
+                            <div class="input-group mb-4 mt-4">
+                                <div class="d-flex estilo-icon-input align-items-center justify-content-center">
+                                    <img class="mx-2" style="height: 15px; width: 15px;" src="img/icons/Vector.png">
+                                </div>
+                                <input type="text" class="quitar-borde-left form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+
+                            <div class="input-group mb-3">
+                                <div class="d-flex estilo-icon-input align-items-center justify-content-center">
+                                    <img class="mx-2" style="height: 15px; width: 15px;" src="img/icons/VectorPass.png">
+                                </div>
+                                <input type="password" class="quitar-borde-left form-control" placeholder="Contraseña" aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+
+
+                            <div class="my-4 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label estilos-links" style="float:left;" for="exampleCheck1">Recordarme</label>
+                            </div>
+
+                            <div class="d-grid gap-2 mb-3">
+                                <button type="submit" class="btn btn-primary" style="background-color: #646CA4;">Ingresar</button>
+                            </div>
+                            
+                            <span>
+                                <p><a class="estilos-links" href="#">¿Olvidaste tu contraseña?</a></p>
+                            </span>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-   </div>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
